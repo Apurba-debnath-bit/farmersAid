e77b7f7fe1c975c11da1dc79439180c52ea4c0ef
Apurba-Debnath-bit <partho.bit999@gmail.com>
2021-11-27 18:40:21 +0600

    change static

--- farmersAid/settings.py
+++ farmersAid/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/abcd/show/'
 MEDIA_ROOT = BASE_DIR
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
