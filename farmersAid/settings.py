13e9e189fedf68028d5299b57c2a7e5d1d313b3d
Apurba Debnath <53510574+Apurba-debnath-bit@users.noreply.github.com>
2022-03-28 04:11:04 +0600

    email change

--- farmersAid/settings.py
+++ farmersAid/settings.py
@@ -151,8 +151,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ekrishok430.com@gmail.com'
-EMAIL_HOST_PASSWORD = 'ekrishok565620'
+EMAIL_HOST_USER = 'farmers.solution001@gmail.com'
+EMAIL_HOST_PASSWORD = 'farmer001'
 
 # # Activate Django-Heroku.
 # django_heroku.settings(locals())
